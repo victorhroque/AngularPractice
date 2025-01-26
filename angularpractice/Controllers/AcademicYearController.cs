15d7702839e821f3bb0f8c4a68c175fd3719fcae
Victor H. Roque <vhcobden@gmail.com>
2025-01-26 15:03:56 -0500

    Add project files.

---
+++ angularpractice/Controllers/AcademicYearController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace angularpractice.Controllers
+{
+    [Route("api/[controller]")]
+    public class AcademicYearController : Controller
+    {
+        private readonly List<AcademicYear> academicYears;
+
+        public AcademicYearController()
+        {
+            academicYears = new List<AcademicYear>
+            {
+                new AcademicYear { AcademicYearId = 1, Name = "2019", StartDate = new DateTime(2019, 1, 1), EndDate = new DateTime(2020, 12, 31) },
+                new AcademicYear { AcademicYearId = 2, Name = "2020", StartDate = new DateTime(2020, 1, 1), EndDate = new DateTime(2021, 12, 31) },
+                new AcademicYear { AcademicYearId = 3, Name = "2021", StartDate = new DateTime(2021, 1, 1), EndDate = new DateTime(2022, 12, 31) }
+            };
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            await Task.Delay(2000);
+            return Ok(academicYears);
+        }
+    }
+
+    public class AcademicYear
+    {
+        public int AcademicYearId { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ angularpractice/Program.cs
@@ -0,0 +1,39 @@
+var allowFrontendAppOrigin = "_allowedOrigins";
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        name: allowFrontendAppOrigin,
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200");
+        });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors(allowFrontendAppOrigin);
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
